00ae366923ffa20ef69d5f417cde67fb5ecd1b98
Uier <ap9940506@gmail.com>
2019-04-24 02:55:01 +0800

    add express build server add loading gamedata.json

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## game for NTNU CSIE summer camp
+- [ ] operations
+- [ ] states
+- [ ] score
+- [ ] cookies
+- [ ] users
--- server.js
+++ server.js
@@ -1,11 +1,43 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+var fs = require('fs');
 var http = require('http');
-
-var server = http.createServer(app);
+var server = http.Server(app);
+var io = require('socket.io')(server);
 
 app.get('/', function(req, res) {
 	console.log(req.url);
-	res.sendFile('/home/uier/game/index.html');
-})
+	res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
 
-server.listen(8888);
+server.listen(8888, () => {
+	console.log(server.address().address);
+	console.log(server.address().port);
+	console.log('Link Start');
+});
+
+let onlineCount = 0;
+
+io.on('connection', (socket) => {
+	onlineCount++;
+
+	io.emit('online', onlineCount);
+
+	socket.on('disconnect', () => {
+		onlineCount = (onlineCount < 0 ? 0 : onlineCount-1);
+		io.emit("online", onlineCount);
+	});
+
+	// socket.on('send', (msg) => {
+	// 	io.emit('msg', msg);
+	// });
+});
+
+if (process.argv.length !== 3) {
+  	console.log('Usage: node server.js [game configuration (json format)]')
+  	process.exit(1);
+}
+
+var gameConfig = fs.readFileSync(process.argv[2], 'ascii');
+gameConfig = JSON.parse(gameConfig);
+console.log(gameConfig);
