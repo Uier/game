e62d773a29b7dcf040dbfb2811d3783184fd2493
Uier <ap9940506@gmail.com>
2019-04-26 23:27:04 +0800

    doMove

--- public/main.js
+++ public/main.js
@@ -1,4 +1,7 @@
+var userList = ['特世大魔王', 'Team1', 'Team2', 'Team3', 'Team4'];
+
 function setCookie(name) {
+	// userList.push(name);
 	var cookie = 'username=' + name + ';';
 	document.cookie = cookie;
 }
@@ -30,12 +33,17 @@ function random_value(max_value) {
 }
 
 var name = checkCookie();
+var cur_rnd = 0, player = 1, finish = 0, used = [false, false, false, false, false];
+var setting = true, data, maximum;
 
 document.addEventListener('DOMContentLoaded', function() {
-
-	socket.on('setgame', function(operation) {
+	socket.on('setgame', function(operation, max_value) {
+		if ( setting == true ) {
+			setting = false;
+			data = operation;
+			maximum = max_value;
+		}
 		var round, prefix, suffix, content;
-		console.log(operation.length);
 		for ( var i=0; i<operation.length; ++i ) {
 			prefix = '<div class="set" id="rnd' + String(i+1) + '">';
 			suffix = '</div>';
@@ -49,10 +57,16 @@ document.addEventListener('DOMContentLoaded', function() {
 			console.log(content);
 			$('#operation').append(content);
 		}
+		$('#operation').append('<div class="set" id="end">End</div>');
 	});
 
-	socket.on('startgame', function(operation, max_value) {
+	socket.on('startgame', function() {
+		finish = 1;
+		startRnd();
+	});
 
+	socket.on('confirm', function(id, container) {
+		doMove(id, container);
 	});
 
 	socket.on('connect', function() {
@@ -71,10 +85,80 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
+var cur_value;
+
+function startRnd() {
+	if ( finish == player ) {
+		console.log('rnd'+String(cur_rnd));
+		$('#rnd' + String(cur_rnd)).css('color', 'white');
+		$('#rnd' + String(cur_rnd)).css('background-color', '#131F37');
+		$('#rnd' + String(cur_rnd+1)).css('color', 'black');
+		$('#rnd' + String(cur_rnd+1)).css('background-color', 'yellow');
+		if ( data[cur_rnd] == '?' ) {
+			if ( random_value(2) == 1 ) {
+				data[cur_rnd] = 'i';
+				$('#rnd' + String(cur_rnd+1)).text('Push');
+				$('#instruction').text('push ');
+				cur_value = random_value(maximum);
+				$('#element').text(String(cur_value) + '.');
+			} else {
+				data[cur_rnd] = 'o';
+				$('#rnd' + String(cur_rnd+1)).text('Pop');
+				$('#instruction').text('pop.');
+				$('#element').text('');
+			}
+		} else if ( data[cur_rnd] == 'i' ) {
+			cur_value = random_value(maximum);
+			$('#instruction').text('push ');
+			$('#element').text(String(cur_value) + '.');
+		} else {
+			$('#instruction').text('pop.');
+			$('#element').text('');
+		}
+		finish = 0;
+		cur_rnd++;
+		for ( var i=0; i<5; ++i )	used[i] = false;
+	}
+}
+
+function findName(element) {
+	return element == name;
+}
+
 function click_queue() {
-    console.log(name + ' choose queue');
+	console.log(name + ' choose queue');
+	var id = userList.findIndex(findName);
+	console.log('click id: '+String(id));
+	if ( id >= 0 && !used[id] ) {
+		console.log('debug: ' + data[cur_rnd-1]);
+		if ( data[cur_rnd-1] == 'i' ) {
+			doMove(id, 'q');
+		} else {
+			socket.emit('check', id, 'q');
+		}
+	}
 }
 
 function click_stack() {
-    console.log(name + ' choose stack');
+	console.log(name + ' choose stack');
+	var id = userList.findIndex(findName);
+	console.log('click id: '+String(id));
+	if ( id >= 0 && !used[id] ) {
+		if ( data[cur_rnd-1] == 'i' ) {
+			doMove(id, 's');
+		} else {
+			socket.emit('check', id, 's');
+		}
+	}
+}
+
+function doMove(id, container) {
+	used[id] = true;
+	finish++;
+	if ( data[cur_rnd-1] == 'i' ) {
+		socket.emit('push', id, container, cur_value);
+	} else {
+		socket.emit('pop', id, container);
+	}
+	startRnd();
 }
--- server.js
+++ server.js
@@ -9,7 +9,6 @@ var fs = require('fs');
 var path = require('path');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var gameModule = require('./gameModule');
 
 app.use('/public', express.static(__dirname+'/public'));
 
@@ -29,21 +28,63 @@ var gameConfig = fs.readFileSync(process.argv[2], 'ascii');
 gameConfig = JSON.parse(gameConfig);
 console.log(gameConfig);
 
+var queue = [];
+var stack = [];
+for ( var i=0; i<5; ++i )	queue[i] = [];
+for ( var i=0; i<5; ++i )	stack[i] = [];
+
 io.on('connection', (socket) => {
 	onlineCount++;
 
 	io.emit('online', onlineCount);
 
-	io.emit('setgame', gameConfig[0]);
+	io.emit('setgame', gameConfig[0], gameConfig[1]);
 
-	io.emit('startgame', gameConfig[0], gameConfig[1]);
+	io.emit('startgame');
 
 	socket.on('disconnect', () => {
 		onlineCount = (onlineCount < 0 ? 0 : onlineCount-1);
 		io.emit('online', onlineCount);
 	});
 
-	// socket.on('submit', () => {
+	socket.on('check', (id, container) => {
+		console.log('check id: ' + String(id));
+		if ( container == 'q' ) {
+			console.log(queue[id].length);
+			if ( queue[id].length > 0 ) {
+				io.emit('confirm', id, container);
+			}
+		} else {
+			if ( stack[id].length > 0 ) {
+				io.emit('confirm', id, container);
+			}
+		}
+	});
 
-	// });
+	socket.on('push', (id, container, element) => {
+		console.log('push id: ' + String(id));
+		if ( container == 'q' ) {
+			queue[id].push(element);
+			console.dir(queue[id]);
+			console.dir(stack[id]);
+		} else {
+			stack[id].push(element);
+			console.dir(queue[id]);
+			console.dir(stack[id]);
+		}
+	});
+
+	socket.on('pop', (id, container) => {
+		console.log('pop id: ' + String(id));
+		if ( container == 'q' ) {
+			queue[id].shift();
+			console.dir(queue[id]);
+			console.dir(stack[id]);
+		} else {
+			stack[id].pop();
+			console.dir(queue[id]);
+			console.dir(stack[id]);
+		}
+	});
+
 });
