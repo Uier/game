38634f4f80d18832e4290ed149ae36d756a8a33f
Uier <ap9940506@gmail.com>
2019-07-06 20:15:59 +0800

    release

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ function random_value(max_value) {
 
 function DoMove(id, act, container) {
 	if ( id >= 0 && !vis[id] ) {
-		vis[id] = 1;
+		vis[id] = true;
 		console.log('click states: ' + vis);
 		if ( act == 'i' ) {
 			if ( container == 'q' ) {
